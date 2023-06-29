f916ce64581772c413cc712056cbf71760f06d0a
Vinil Vadakkepurakkal <vinilv@microsoft.com>
2023-06-29 14:54:30 +0530

    Update 000_lustre_client_install.sh

--- specs/default/cluster-init/scripts/000_lustre_client_install.sh
+++ specs/default/cluster-init/scripts/000_lustre_client_install.sh
@@ -5,6 +5,7 @@
 #Env - Azure CycleCloud
 
 OS_VER=$(jetpack config platform_family)
+LUSTRE_VER=$(jetpack config amlfs.client_ver)
 case $OS_VER in 
 rhel)
    
@@ -18,6 +19,6 @@ rhel)
         echo -e "gpgcheck=1" >> ${REPO_PATH}
         echo -e "gpgkey=https://packages.microsoft.com/keys/microsoft.asc" >> ${REPO_PATH}
         yum clean all
-        sudo yum install amlfs-lustre-client-2.15.1_29_gbae0abe-$(uname -r | sed -e "s/\.$(uname -p)$//" | sed -re 's/[-_]/\./g')-1
+        sudo yum install amlfs-lustre-client-$(LUSTRE_VER)-$(uname -r | sed -e "s/\.$(uname -p)$//" | sed -re 's/[-_]/\./g')-1
         ;;
 esac
