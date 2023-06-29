a6bdb9e965079111a08da0964f26bd5b0bcb1d19
Vinil Vadakkepurakkal <vinilv@microsoft.com>
2023-06-29 14:51:24 +0530

    Create 001_mount.sh

---
+++ specs/default/cluster-init/scripts/001_mount.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Author : Vinil Vadakkepurakkal
+# Integrating Azure Managed Lustre filesystem in CycleCloud
+#OS Tested : CentOS 7 / RHEL7 / Alma Linux 8 / Ubuntu 18.04
+#Env - Azure CycleCloud
+MOUNT_POINT=$(jetpack config amlfs.mount_point)
+MGS_IP=$(jetpack config amlfs.mgs_ip)
+mkdir -p $MOUNT_POINT
+sudo mount -t lustre -o noatime,flock $MGS_IP@tcp:/lustrefs $MOUNT_POINT
