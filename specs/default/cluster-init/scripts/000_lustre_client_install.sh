4c66cd9de3e8d55ebcbab194bf59e0e6db9bee16
Vinil Vadakkepurakkal <vinilv@microsoft.com>
2023-06-29 13:06:54 +0530

    AMLFS template + Script

---
+++ specs/default/cluster-init/scripts/000_lustre_client_install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#Author : Vinil Vadakkepurakkal
+# Integrating Azure Managed Lustre filesystem in CycleCloud
+#OS Tested : CentOS 7 / RHEL7 / Alma Linux 8 / Ubuntu 18.04
+#Env - Azure CycleCloud
+
+OS_VER=$(jetpack config platform_family)
+case $OS_VER in 
+rhel)
+   
+        rpm --import https://packages.microsoft.com/keys/microsoft.asc  
+        DISTRIB_CODENAME=el7
+        REPO_PATH=/etc/yum.repos.d/amlfs.repo
+        echo -e "[amlfs]" > ${REPO_PATH}
+        echo -e "name=Azure Lustre Packages" >> ${REPO_PATH}
+        echo -e "baseurl=https://packages.microsoft.com/yumrepos/amlfs-${DISTRIB_CODENAME}" >> ${REPO_PATH}
+        echo -e "enabled=1" >> ${REPO_PATH}
+        echo -e "gpgcheck=1" >> ${REPO_PATH}
+        echo -e "gpgkey=https://packages.microsoft.com/keys/microsoft.asc" >> ${REPO_PATH}
+        ;;
+esac
