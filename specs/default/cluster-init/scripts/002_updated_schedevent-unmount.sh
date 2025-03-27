c075d2c14d69c5a081705943ae793f4ab91b49f5
Vinil Vadakkepurakkal <82436391+vinil-v@users.noreply.github.com>
2025-03-27 20:25:41 +0530

    Update 002_updated_schedevent-unmount.sh

--- specs/default/cluster-init/scripts/002_updated_schedevent-unmount.sh
+++ specs/default/cluster-init/scripts/002_updated_schedevent-unmount.sh
@@ -9,48 +9,50 @@
 # Terminate: The virtual machine is scheduled to be deleted.
 
 mkdir -p /opt/cycle/jetpack/scripts
+
 cat >> /opt/cycle/jetpack/scripts/onTerminate.sh << 'EOF'
 #!/bin/sh
 LOGFILE=/sched/unmount_lustre.log
 MOUNTPOINT=$(jetpack config amlfs.mount_point)
-echo "[INFO] Starting unmount process for $MOUNTPOINT" | tee -a "$LOGFILE"
+echo "`date "+%D %T"`[INFO] Starting unmount process for $MOUNTPOINT" | tee -a "$LOGFILE"
 
 while mountpoint -q $MOUNTPOINT; do
-    echo "[WARNING] $MOUNTPOINT is still mounted. Checking processes..." | tee -a "$LOGFILE"
+    echo "`date "+%D %T"` [WARNING] $MOUNTPOINT is still mounted. Checking processes..." | tee -a "$LOGFILE"
     /sbin/fuser -vm $MOUNTPOINT 2>&1 | tee -a "$LOGFILE"
-    echo "[INFO] Sending TERM signal to processes..." | tee -a "$LOGFILE"
+    echo "`date "+%D %T"` [INFO] Sending TERM signal to processes..." | tee -a "$LOGFILE"
     /sbin/fuser -kMm -TERM $MOUNTPOINT
     /bin/sleep 5
-    echo "[INFO] Sending KILL signal to remaining processes..." | tee -a "$LOGFILE"
+    echo "`date "+%D %T"` [INFO] Sending KILL signal to remaining processes..." | tee -a "$LOGFILE"
     /sbin/fuser -kMm -KILL $MOUNTPOINT
     /bin/sleep 5
-    echo "[INFO] Attempting to unmount $MOUNTPOINT" | tee -a "$LOGFILE"
+    echo "`date "+%D %T"` [INFO] Attempting to unmount $MOUNTPOINT" | tee -a "$LOGFILE"
     /bin/umount $MOUNTPOINT
     /bin/sleep 5
 done
 
-echo "[SUCCESS] $MOUNTPOINT successfully unmounted `hostname`." | tee -a "$LOGFILE"
+echo "`date "+%D %T"` [SUCCESS] $MOUNTPOINT successfully unmounted `hostname`." | tee -a "$LOGFILE"
 EOF
 cat >>/opt/cycle/jetpack/scripts/onPreempt.sh << EOF
 #!/bin/sh
 LOGFILE=/sched/unmount_lustre.log
 MOUNTPOINT=$(jetpack config amlfs.mount_point)
-echo "[INFO] Starting unmount process for $MOUNTPOINT" | tee -a "$LOGFILE"
+echo "`date "+%D %T"`[INFO] Starting unmount process for $MOUNTPOINT" | tee -a "$LOGFILE"
 
 while mountpoint -q $MOUNTPOINT; do
-    echo "[WARNING] $MOUNTPOINT is still mounted. Checking processes..." | tee -a "$LOGFILE"
+    echo "`date "+%D %T"` [WARNING] $MOUNTPOINT is still mounted. Checking processes..." | tee -a "$LOGFILE"
     /sbin/fuser -vm $MOUNTPOINT 2>&1 | tee -a "$LOGFILE"
-    echo "[INFO] Sending TERM signal to processes..." | tee -a "$LOGFILE"
+    echo "`date "+%D %T"` [INFO] Sending TERM signal to processes..." | tee -a "$LOGFILE"
     /sbin/fuser -kMm -TERM $MOUNTPOINT
     /bin/sleep 5
-    echo "[INFO] Sending KILL signal to remaining processes..." | tee -a "$LOGFILE"
+    echo "`date "+%D %T"` [INFO] Sending KILL signal to remaining processes..." | tee -a "$LOGFILE"
     /sbin/fuser -kMm -KILL $MOUNTPOINT
     /bin/sleep 5
-    echo "[INFO] Attempting to unmount $MOUNTPOINT" | tee -a "$LOGFILE"
+    echo "`date "+%D %T"` [INFO] Attempting to unmount $MOUNTPOINT" | tee -a "$LOGFILE"
     /bin/umount $MOUNTPOINT
     /bin/sleep 5
 done
-echo "[SUCCESS] $MOUNTPOINT successfully unmounted from `hostname`." | tee -a "$LOGFILE"
+
+echo "`date "+%D %T"` [SUCCESS] $MOUNTPOINT successfully unmounted `hostname`." | tee -a "$LOGFILE"
 EOF
 chmod +x /opt/cycle/jetpack/scripts/onTerminate.sh
 chmod +x /opt/cycle/jetpack/scripts/onPreempt.sh
